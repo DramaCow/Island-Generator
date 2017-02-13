ae06d9ce8973ccd547516d6dd9915bfb6db24178
DramaCow <sc14809@my.bristol.ac.uk>
2017-02-13 14:52:41 +0000

    Getting there

--- islgen.py
+++ islgen.py
@@ -5,56 +5,8 @@ import time
 import random
 from scipy.spatial import Voronoi
 from noise import pnoise2
-
-# Plot voronoi tesselation
-def draw_2d_voronoi(vor, land):
-  fig = plt.figure()
-  plt.hold(True)
-
-  # Mark the points
-  plt.plot(vor.points[:,0], vor.points[:, 1], 'wo', markersize=2)
-  
-  # Mark the Voronoi vertices.
-  plt.plot(vor.vertices[:,0], vor.vertices[:, 1], 'ko', markersize=6)
-  
-  for vpair in vor.ridge_vertices:
-    if vpair[0] >= 0 and vpair[1] >= 0:
-      v0 = vor.vertices[vpair[0]]
-      v1 = vor.vertices[vpair[1]]
-      # Draw a line from v0 to v1.
-      plt.plot([v0[0], v1[0]], [v0[1], v1[1]], 'k', linewidth=2)
-  
-  plt.xlim([-0.25, 1.25])
-  plt.ylim([-0.25, 1.25])
-
-  for i, r in enumerate(vor.point_region):
-    if land[i] == 1:
-      polygon = [vor.vertices[v] for v in vor.regions[r]]
-      plt.fill(*zip(*polygon), color="green")
-  
-  plt.hold(False)
-  
-  plt.show()
-
-# Export to .obj file
-def output_obj(vor, land):
-  with open("mesh.obj", "w") as f :
-    f.write("# OBJ file\n")
-    for v in vor.vertices:
-      f.write("v %.4f %.4f %.4f\n" % (v[0], 0, v[1]))
-    for v in vor.vertices:
-      f.write("vn %.0f %.0f %.0f\n" % (0, 1, 0))
-    for p, l in enumerate(land):
-      if l == 1:
-        f.write("f")
-        for i in vor.regions[vor.point_region[p]]:
-          f.write(" %d" % (i + 1)) # vertices indexed-by-1 rather than 0
-        f.write("\n")  
-
-# Voronoi relaxation (Lloyd's Algorithm)
-def relax(vor):
-  points = [np.mean([vor.vertices[v] for v in vor.regions[r]], axis=0) for r in vor.point_region]
-  return Voronoi(points)
+import math
+from matplotlib.widgets import CheckButtons
 
 # ============
 # === MAIN ===
@@ -79,7 +31,8 @@ random.seed(arg_seed)
 # Generate relaxed voronoi tesselation
 vor = Voronoi(np.random.rand(arg_points,2))
 for i in range(0, arg_relax):
-  vor = relax(vor)
+  # Voronoi relaxation (Lloyd's Algorithm)
+  vor = Voronoi([np.mean([vor.vertices[v] for v in vor.regions[r]], axis=0) for r in vor.point_region])
 
 # Assign subset of polygons to be part of island
 land = np.zeros(len(vor.points))
@@ -88,10 +41,61 @@ for i, p in enumerate(vor.points):
     arg_span = 4
     x, y = int(p[0]*255), int(p[1]*255)
     c = pnoise2(x*arg_span/256, y*arg_span/256)
-    land[i] = c > 0.0 # 0.3*0.3*p.dot(p)
+    land[i] = c > 0.3*0.3*p.dot(p) 
 
-# Export as .obj file
-output_obj(vor, land)
+# ===========================
+# === Export as .obj file ===
+# ===========================
 
-# Matplotlib print
-draw_2d_voronoi(vor, land)
+with open("mesh.obj", "w") as f :
+  f.write("# OBJ file\n")
+  for v in vor.vertices:
+    f.write("v %.4f %.4f %.4f\n" % (v[0], 0, v[1]))
+  for p, l in enumerate(land):
+    if l == 1:
+      point = vor.points[p]
+      region = vor.regions[vor.point_region[p]]
+      f.write("f")
+      for vi in region:
+        f.write(" %d" % (vi + 1)) # vertices indexed-by-1 rather than 0
+      f.write("\n")  
+
+# ====================
+# === PLOT VORONOI ===
+# ====================
+
+fig, ax = plt.subplots()
+plt.hold(True)
+
+def onpick(event):
+  ind = event.ind
+  print('onpick scatter:', ind)
+  land[ind] = 1-land[ind]
+  if land[i] == 1:
+    polygon = [vor.vertices[v] for v in vor.regions[r]]
+    ax.fill(*zip(*polygon), color="green")
+
+# Mark the points
+ax.plot(vor.points[:,0], vor.points[:, 1], 'wo', markersize=8, picker=8)
+fig.canvas.mpl_connect('pick_event', onpick)
+  
+# Mark the Voronoi vertices.
+ax.plot(vor.vertices[:,0], vor.vertices[:, 1], 'ko', markersize=6)
+  
+for vpair in vor.ridge_vertices:
+  if vpair[0] >= 0 and vpair[1] >= 0:
+    v0 = vor.vertices[vpair[0]]
+    v1 = vor.vertices[vpair[1]]
+    # Draw a line from v0 to v1.
+    ax.plot([v0[0], v1[0]], [v0[1], v1[1]], 'k', linewidth=2)
+  
+ax.set_xlim([-0.25, 1.25])
+ax.set_ylim([-0.25, 1.25])
+
+for i, r in enumerate(vor.point_region):
+  if land[i] == 1:
+    polygon = [vor.vertices[v] for v in vor.regions[r]]
+    ax.fill(*zip(*polygon), color="green")
+
+plt.hold(False)
+plt.show()
