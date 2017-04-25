70f51efdabbc9bd1b11fe3927d43818e0a68ca7f
DramaCow <sc14809@my.bristol.ac.uk>
2017-04-25 13:17:13 +0100

    radial based randomisation yields more exciting results

--- islgen.py
+++ islgen.py
@@ -7,6 +7,7 @@ from scipy.spatial import Voronoi
 from noise import pnoise2
 import math
 from matplotlib.widgets import CheckButtons
+from matplotlib.animation import FuncAnimation
 
 # ============
 # === MAIN ===
@@ -34,14 +35,20 @@ for i in range(0, arg_relax):
   # Voronoi relaxation (Lloyd's Algorithm)
   vor = Voronoi([np.mean([vor.vertices[v] for v in vor.regions[r]], axis=0) for r in vor.point_region])
 
+R = np.random.randint(0, 200, 12)
+
 # Assign subset of polygons to be part of island
 land = np.zeros(len(vor.points))
 for i, p in enumerate(vor.points):
   if (0 <= p[0] <= 1) and (0 <= p[1] <= 1) and -1 not in vor.regions[vor.point_region[i]]:
     arg_span = 4
     x, y = int(p[0]*255), int(p[1]*255)
-    c = pnoise2(x*arg_span/256, y*arg_span/256)
-    land[i] = c > 0.3*0.3*p.dot(p) 
+		#c = pnoise2(x*arg_span/256, y*arg_span/256)
+    #land[i] = c > 0.3*0.3*p.dot(p) 
+    c = math.sqrt((x - 128)**2 + (y - 128)**2)
+    #land[i] = c < 80 + np.random.randint(-70, 70)
+    a = int(math.atan2(y - 128, x - 128) / (math.pi / 6))
+    land[i] = c < R[a]
 
 # ===========================
 # === Export as .obj file ===
@@ -64,44 +71,42 @@ with open("mesh.obj", "w") as f :
 # === PLOT VORONOI ===
 # ====================
 
-fig, ax = plt.subplots()
-
-def fill_polygons(vor, land):
-  for i, r in enumerate(vor.point_region):
-    if land[i] == 1:
-      polygon = [vor.vertices[v] for v in vor.regions[r]]
-      ax.fill(*zip(*polygon), color="green")
-
-def plot_voronoi(vor, land):
-  plt.hold(True)
-  
-  # Mark the points
-  ax.plot(vor.points[:,0], vor.points[:, 1], 'wo', markersize=4, picker=8)
-  fig.canvas.mpl_connect('pick_event', onpick)
-    
-  # Mark the Voronoi vertices.
-  ax.plot(vor.vertices[:,0], vor.vertices[:, 1], 'ko', markersize=6)
-    
-  for vpair in vor.ridge_vertices:
-    if vpair[0] >= 0 and vpair[1] >= 0:
-      v0 = vor.vertices[vpair[0]]
-      v1 = vor.vertices[vpair[1]]
-      # Draw a line from v0 to v1.
-      ax.plot([v0[0], v1[0]], [v0[1], v1[1]], 'k', linewidth=2)
-    
-  ax.set_xlim([-0.25, 1.25])
-  ax.set_ylim([-0.25, 1.25])
-  
-  fill_polygons(vor, land)
-  
-  plt.hold(False)
-  plt.show()
-
 def onpick(event):
   ind = event.ind
+  print(ind)
   land[ind] = 1-land[ind]
-  #ax.clear()
-  plot_voronoi(vor, land)
-  fig.canvas.draw()
+  if land[ind] == 1:
+    polygon = [vor.vertices[v] for v in vor.regions[vor.point_region[ind]]]
+    ax.fill(*zip(*polygon), color="green")
 
-plot_voronoi(vor, land)
+def fill_polygons(i):
+  if land[ind] == 1:
+    polygon = [vor.vertices[v] for v in vor.regions[vor.point_region[ind]]]
+    ax.fill(*zip(*polygon), color="green")
+
+fig, ax = plt.subplots()
+  
+# Mark the points
+ax.plot(vor.points[:,0], vor.points[:, 1], 'wo', markersize=4, picker=8)
+fig.canvas.mpl_connect('pick_event', onpick)
+    
+# Mark the Voronoi vertices.
+ax.plot(vor.vertices[:,0], vor.vertices[:, 1], 'ko', markersize=6)
+    
+for vpair in vor.ridge_vertices:
+  if vpair[0] >= 0 and vpair[1] >= 0:
+    v0 = vor.vertices[vpair[0]]
+    v1 = vor.vertices[vpair[1]]
+    # Draw a line from v0 to v1.
+    ax.plot([v0[0], v1[0]], [v0[1], v1[1]], 'k', linewidth=2)
+    
+ax.set_xlim([-0.25, 1.25])
+ax.set_ylim([-0.25, 1.25])
+
+for i, r in enumerate(vor.point_region):
+  if land[i] == 1:
+    polygon = [vor.vertices[v] for v in vor.regions[r]]
+    ax.fill(*zip(*polygon), color="green")
+  
+#animation = FuncAnimation(fig, fill_polygons, interval=10)
+plt.show()
